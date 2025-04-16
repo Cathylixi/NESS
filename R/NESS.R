d8263095a126c409d882408a87722398b08a5ba6
jasper <jingyuanhu@Jingyuans-MacBook-Pro.local>
2025-04-16 00:29:56 -0400

    update package

--- R/NESS.R
+++ R/NESS.R
@@ -47,6 +47,7 @@
 #' @importFrom phateR phate
 #' @importFrom RSpectra svds
 #' @importFrom BiocNeighbors findKNN
+#' @importFrom stats median
 #' @export
 
 
@@ -56,19 +57,19 @@ NESS <- function(
     data.name = "",
     GCP = NULL,
     cluster = NULL,
-    rareness = FALSE,
     method = "tsne",
     initialization = 1,
     stability_threshold = 0.75,
-    early_stop = TRUE,
+    early_stop = FALSE,
     seed_base = 1,
     N = 20,
     k = 50,
     svd_cutoff_ratio = 1.1,
     svd_max_k = 30,
-    stop_global_stability_threshold = 0.9,
-    stop_relative_change_threshold = 0.05
+    stop_global_stability_threshold = 0.98,
+    stop_relative_change_threshold = 0.02
 ) {
+  # Setup parameters
   set.seed(seed_base)
   eg.v <- svds((data), k = svd_max_k)$d
   pc <- max(which(eg.v[1:(svd_max_k - 1)] / eg.v[2:svd_max_k] > svd_cutoff_ratio))
@@ -80,201 +81,138 @@ NESS <- function(
   }
 
   global_stability <- c()
-  rare.mean <- c()
+  rare.median <- c()
   rare.var <- c()
-  all_embedding <- list()
-  plot_list_cluster <- list()
-  plot_list_rareness <- list()
+  rare_violin <- list()
+  best_embedding <- list()
+  embedding_plot_list <- list()
   plot_list_stability <- list()
   local_stability <- list()
-  init_type <- ifelse(initialization == 2, "PCA Initialization", "Random Initialization")
   prev_stability <- NULL
-  tracking_GCP_length <- 0
 
+  # Running NESS main analysis
   for (j in seq_along(GCP)) {
     cat(paste0("Running GCP value: ", GCP[j], " (", j, "/", length(GCP), ")\n"))
-    tracking_GCP_length <- j
     gcp <- GCP[j]
     set.seed(seed_base + j)
     knn.mat <- list()
     knn.info <- NULL
+    all_embeddings <- list()
 
-    if (method == "tsne") {
-      for (i in seq_len(N)) {
-        if (i %% 10 == 0 || i == N) {
-          cat(paste0("  TSNE iteration: ", i, "/", N, "\n"))
-        }
-        out <- Rtsne(data.denoise,
-                          perplexity = gcp,
-                          check_duplicates = FALSE,
-                          pca = (initialization == 2),
-                          seed = seed_base + i,
-                          ...)
-        embedding <- out$Y
-        knn.mat[[i]] <- findKNN(embedding, k = k)$index
-      }
+    for (i in seq_len(N)) {
+      if (i %% 10 == 0 || i == N) cat(paste0("  ", toupper(method), " iteration: ", i, "/", N, "\n"))
+      set.seed(seed_base + i)
+
+      embedding <- switch(method,
+                          tsne = Rtsne(data.denoise, perplexity = gcp, check_duplicates = FALSE, pca = (initialization == 2), seed = seed_base + i, ...)$Y,
+                          umap = {
+                            if (is.null(knn.info)) {
+                              out <- uwot::umap(data.denoise, n_neighbors = gcp, init = ifelse(initialization == 2, "pca", "lvrandom"), n_threads = 1, ret_nn = TRUE, ...)
+                              knn.info <- list(idx = out$nn$euclidean$idx, dist = out$nn$euclidean$dist)
+                              out$embedding
+                            } else {
+                              uwot::umap(data.denoise, n_neighbors = gcp, init = ifelse(initialization == 2, "pca", "lvrandom"), n_threads = 1, nn_method = knn.info, ret_nn = FALSE, ...)
+                            }
+                          },
+                          phateR = {
+                            if (i == 1) phate.init <- NULL
+                            out <- phate(data.denoise, ndim = 2, knn = gcp, seed = seed_base + i, verbose = TRUE, init = if (exists("phate.init")) phate.init else NULL, ...)
+                            if (i == 1) phate.init <- out
+                            out$embedding
+                          },
+                          stop("Unsupported method")
+      )
+      all_embeddings[[i]] <- embedding
+      knn.mat[[i]] <- findKNN(embedding, k = k)$index
     }
-   else if (method == "umap") {
-      for (i in seq_len(N)) {
-        if (i %% 10 == 0 || i == N) {
-          cat(paste0("  UMAP iteration: ", i, "/", N, "\n"))
-        }
-        set.seed(seed_base + i)
-        if (is.null(knn.info)) {
-          out <- uwot::umap(data.denoise,
-                            n_neighbors = gcp,
-                            init = ifelse(initialization == 2, "pca", "lvrandom"),
-                            n_threads = 1,
-                            ret_nn = TRUE,
-                            ...)
-          knn.info <- list(idx = out$nn$euclidean$idx, dist = out$nn$euclidean$dist)
-          embedding <- out$embedding
-        } else {
-          embedding <- uwot::umap(data.denoise,
-                                  n_neighbors = gcp,
-                                  init = ifelse(initialization == 2, "pca", "lvrandom"),
-                                  n_threads = 1,
-                                  nn_method = knn.info,
-                                  ret_nn = FALSE,
-                                  ...)
-        }
-        knn.mat[[i]] <- findKNN(embedding, k = k)$index
-      }
-    }
-    else if (method == "phateR") {
-      phate.init <- NULL
-      for (i in seq_len(N)) {
-        if (i %% 10 == 0 || i == N) {
-          cat(paste0("  PHATE iteration: ", i, "/", N, "\n"))
-        }
-        set.seed(seed_base + i)
-        out <- phate(data.denoise,
-                     ndim = 2,
-                     knn = gcp,
-                     seed = seed_base + i,
-                     verbose = TRUE,
-                     init = phate.init,
-                     ...)
-        if (i == 1) phate.init <- out
-        embedding <- out$embedding
-        knn.mat[[i]] <- findKNN(embedding, k = k)$index
+
+    # Compute rareness score
+    temp.out <- matrix(ncol = N, nrow = N)
+    for (p in 1:(N - 1)) {
+      for (q in (p + 1):N) {
+        temp <- sapply(1:nrow(knn.mat[[1]]), function(mn) {
+          length(intersect(knn.mat[[p]][mn, 1:k], knn.mat[[q]][mn, 1:k])) / k
+        })
+        temp.out[p, q] <- median(temp)
       }
     }
 
-    Y <- embedding
+    rareness_mean <- sapply(1:N, function(aa) {
+      if (aa == 1) {
+        mean(temp.out[1, 2:N])
+      } else if (aa == N) {
+        mean(temp.out[1:(N - 1), N])
+      } else {
+        mean(c(temp.out[1:(aa - 1), aa], temp.out[aa, (aa + 1):N]))
+      }
+    })
+    rareness_variance <- sapply(1:N, function(aa) {
+      temp_list <- if (aa == 1) temp.out[1, 2:N]
+      else if (aa == N) temp.out[1:(N - 1), N]
+      else c(temp.out[1:(aa - 1), aa], temp.out[aa, (aa + 1):N])
+      sum((temp_list - rareness_mean[aa])^2) / length(temp_list)
+    })
 
+    rare.median[j] <- median(temp.out[upper.tri(temp.out)], na.rm = TRUE)
+    rare.var[j] <- var(temp.out[upper.tri(temp.out)], na.rm = TRUE)
+    rare_violin[[j]] <- plot_rareness_violin(temp.out, gcp)
+
+    # Find the best embedding
+    best_embedding_index <- which.min(rareness_mean)
+    best_embedding[[j]] <- all_embeddings[[best_embedding_index]]
+    embedding_plot_list[[j]] <- plot_embedding(best_embedding[[j]], cluster, data.name, method, gcp, pc)
+
+    # Calculate stability score
     knn.graph <- matrix(0, nrow = nrow(knn.mat[[1]]), ncol = nrow(knn.mat[[1]]))
     for (i in seq_len(N)) {
       for (l in seq_len(nrow(knn.mat[[1]]))) {
         knn.graph[l, knn.mat[[i]][l, 1:k]] <- knn.graph[l, knn.mat[[i]][l, 1:k]] + 1
       }
-      knn.graph <- pmax(knn.graph, t(knn.graph))
     }
+    knn.graph <- pmax(knn.graph, t(knn.graph))
 
     knn.score <- sapply(seq_len(nrow(knn.mat[[1]])), function(i) {
       quantile(knn.graph[i, knn.graph[i, ] != 0] / N, stability_threshold)
     })
     names(knn.score) <- NULL
     local_stability[[j]] <- knn.score
-
-    all_embedding[[j]] <- Y
-
-    if (rareness) {
-      temp.out <- matrix(ncol = N, nrow = N)
-      for (p in 1:(N - 1)) {
-        for (q in (p + 1):N) {
-          temp <- sapply(1:nrow(knn.mat[[1]]), function(mn) {
-            length(intersect(knn.mat[[p]][mn, 1:k], knn.mat[[q]][mn, 1:k])) / k
-          })
-          temp.out[p, q] <- median(temp)
-        }
-      }
-      rareness_mean <- sapply(1:N, function(aa) {
-        if (aa == 1) {
-          mean(temp.out[1, 2:N])
-        } else if (aa == N) {
-          mean(temp.out[1:(N - 1), N])
-        } else {
-          mean(c(temp.out[1:(aa - 1), aa], temp.out[aa, (aa + 1):N]))
-        }
-      })
-      rareness_variance <- sapply(1:N, function(aa) {
-        temp_list <- if (aa == 1) temp.out[1, 2:N]
-        else if (aa == N) temp.out[1:(N - 1), N]
-        else c(temp.out[1:(aa - 1), aa], temp.out[aa, (aa + 1):N])
-        sum((temp_list - rareness_mean[aa])^2) / length(temp_list)
-      })
-
-      rare.mean[j] <- median(temp.out[upper.tri(temp.out)], na.rm = TRUE)
-      rare.var[j] <- var(temp.out[upper.tri(temp.out)], na.rm = TRUE)
-    }
-
-    if (!is.null(cluster)) {
-      plot.data <- data.frame(dim1 = Y[, 1], dim2 = Y[, 2], cluster = cluster)
-      plot11 <- ggplot(plot.data, aes(dim1, dim2, colour = cluster)) +
-        geom_point(size = 1) +
-        ggtitle(paste0(data.name, "_", method, "(", init_type, ")" , "_p", gcp, "_pc", pc, "_colored by cluster"))
-      plot_list_cluster[[j]] <- plot11
-    }
-
-    plot.data <- data.frame(dim1 = Y[, 1], dim2 = Y[, 2], knn_score = knn.score)
-    plot22 <- ggplot(plot.data, aes(dim1, dim2, colour = knn_score)) +
-      geom_point(size = 1) +
-      ggtitle(paste0(data.name, "_", method, "(", init_type, ")" , "_p", gcp, "_pc", pc, "_colored by local stability score"))
-    plot_list_stability[[j]] <- plot22
+    plot_list_stability[[j]] <- plot_embedding_stability(best_embedding[[j]], knn.score, data.name, method, gcp)
 
     global_stability[j] <- mean(knn.score, na.rm = TRUE)
-
     print(paste("GCP =", GCP[j], "→ Global Stability =", round(global_stability[j], 4)))
-    if (!is.null(prev_stability)) {
-      relative_change <- (global_stability[j] - prev_stability) / abs(prev_stability)
-      prev_stability <- global_stability[j]
+
+    stability_plot <- plot_line(GCP[1:length(global_stability)], global_stability, "Global Stability", paste0(data.name, method, " - Global Stability"))
 
 
-      if (early_stop && (global_stability[j] >= stop_global_stability_threshold || relative_change <= stop_relative_change_threshold)) {
-        break
-      }
+    # Find the best GCP score
+    if (any(global_stability > stop_global_stability_threshold)) {
+      best_gcp_idx <- which(global_stability > stop_global_stability_threshold)[1]
+      if (early_stop) break
+    } else if (any(diff(global_stability) < stop_relative_change_threshold)) {
+      best_gcp_idx <- which(diff(global_stability) < stop_relative_change_threshold)[1] + 1
+      if (early_stop) break
+    } else {
+      best_gcp_idx <- length(global_stability)
     }
-  }
-
-  names(global_stability) <- GCP
-  rare.mean.vec <- unlist(rare.mean)[1:tracking_GCP_length]
-  med_value <- median(rare.mean.vec, na.rm = TRUE)
-  med_index <- which.min(abs(rare.mean.vec - med_value))
-  print(paste("Index of MEDIAN rareness score:", med_index))
-  print(paste("Corresponding GCP value for median rareness score:", GCP[med_index]))
-
-  plot_improved <- function(x, y, y_label, title) {
-    df <- data.frame(x = x, y = y)
-    ggplot(df, aes(x = x, y = y)) +
-      geom_line(color = "blue") +
-      geom_point(size = 2) +
-      xlab("GCP") +
-      ylab(y_label) +
-      ggtitle(title) +
-      theme_minimal()
-  }
-
-  stability_plot <- plot_improved(GCP[1:tracking_GCP_length], global_stability, "Global Stability", paste0(data.name, method, " - Global Stability (", init_type, ")"))
-
-  if (early_stop) {
-    message <- paste("The final GCP is", GCP[med_index], "with median rareness score =", rare.mean.vec[med_index])
-    print(message)
   }
 
   result_list <- list(
     GCP = GCP,
-    GCP.optim = GCP[med_index],
-    embedding = all_embedding[[med_index]],
-    local_stability = local_stability[[med_index]],
+    GCP.optim = GCP[best_gcp_idx],
+    embedding = best_embedding[[best_gcp_idx]],
+    embedding_plot = embedding_plot_list[[best_gcp_idx]],
+    local_stability = local_stability[[best_gcp_idx]],
     global_stability = global_stability,
-    embedding_stability_colored = plot_list_stability[[med_index]],
+    embedding_stability_colored = plot_list_stability[[best_gcp_idx]],
     global_stability_plot = stability_plot,
+    rare.median = rare.median,
+    rare.var = rare.var,
+    rare_violin = rare_violin[[best_gcp_idx]],
     par = list(
       data.name = data.name,
       GCP = GCP,
       cluster = cluster,
-      rareness = rareness,
+      rareness = TRUE,
       method = method,
       initialization = initialization,
       stability_threshold = stability_threshold,
@@ -288,34 +226,69 @@ NESS <- function(
       stop_relative_change_threshold = stop_relative_change_threshold
     )
   )
-  if (!is.null(cluster)) {
-    result_list$embedding_cluster_colored <- plot_list_cluster[[med_index]]
-  }
-  if (rareness) {
-    result_list$rareness_mean_plot <- plot_improved(GCP[1:tracking_GCP_length], rare.mean, "Rareness Score(Mean)", paste0(data.name, method, " - Rareness Score(Mean) (", init_type, ")"))
-
-    names(rare.mean) <- GCP
-    names(rare.var) <- GCP
-    # Create the plot
-    result_list$rareness_boxplot <- ggplot(plot_data <- data.frame(
-                                              GCP = factor(GCP, levels = GCP),
-                                              mean = as.numeric(rare.mean),
-                                              sd = sqrt(as.numeric(rare.var))
-                                            ),
-                               aes(x = GCP, y = mean)) +
-      geom_point(size = 3) +
-      geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.2) +
-      labs(
-        title = "Rareness Score accross GCP",
-        x = "GCP",
-        y = "Median Embedding Rareness Score"
-      ) +
-      theme_minimal()
-
-
-    result_list$rare.mean = rare.mean
-    result_list$rare.var = rare.var
-  }
-
   return(result_list)
 }
+
+
+# Plot helper for stability embedding
+plot_embedding_stability <- function(embedding, knn_score, data.name, method, gcp) {
+  plot.data <- data.frame(dim1 = embedding[, 1], dim2 = embedding[, 2], knn_score = knn_score)
+  ggplot(plot.data, aes(dim1, dim2, colour = knn_score)) +
+    geom_point(size = 1) +
+    ggtitle(paste0(data.name, "_", method, "_p", gcp, "_colored by local stability score")) +
+    theme_minimal()
+}
+
+# Plot helper for cluster-colored embedding
+plot_embedding <- function(embedding, cluster, data.name, method, gcp, pc) {
+  plot.data <- data.frame(dim1 = embedding[, 1], dim2 = embedding[, 2])
+
+  if (!is.null(cluster)) {
+    plot.data$cluster <- cluster
+    ggplot(plot.data, aes(dim1, dim2, colour = cluster)) +
+      geom_point(size = 1) +
+      ggtitle(paste0(data.name, "_", method, "_p", gcp, "_pc", pc, "_colored by cluster")) +
+      theme_minimal()
+  } else {
+    ggplot(plot.data, aes(dim1, dim2)) +
+      geom_point(size = 1, color = "black") +
+      ggtitle(paste0(data.name, "_", method, "_p", gcp, "_pc", pc)) +
+      theme_minimal()
+  }
+}
+
+
+# Generic plot helper for line plots
+plot_line <- function(x, y, y_label, title) {
+  df <- data.frame(x = x, y = y)
+  ggplot(df, aes(x = x, y = y)) +
+    geom_line(color = "blue") +
+    geom_point(size = 2) +
+    xlab("GCP") +
+    ylab(y_label) +
+    ggtitle(title) +
+    theme_minimal()
+}
+
+# Rareness boxplot helper
+plot_rareness_violin <- function(temp.out, gcp) {
+  df <- data.frame(value = temp.out[upper.tri(temp.out)],
+                   gcp = paste0("GCP=", gcp))
+
+  ggplot(df, aes(x = gcp, y = value)) +
+    geom_violin(fill = "steelblue", color = "black", alpha = 0.8) +
+    labs(
+      title = paste0("Violin Plot of Rareness Score", "_p", gcp),
+      x = "",  # You could put "GCP" if you want to label the axis generally
+      y = "Pairwise Rareness Score"
+    ) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(size = 14, face = "bold"),
+      axis.title = element_text(size = 12)
+    )
+}
+
+
+
+
